dcae0af2b16dc1e108e8937a0ef83c7ed2522d6f
John Ink <kujohnink@gmail.com>
2021-02-24 09:35:38 -0600

    Update comments in AP_AOA files.

--- libraries/AP_AOA/AP_AOA.cpp
+++ libraries/AP_AOA/AP_AOA.cpp
@@ -1,21 +1,8 @@
-/****************************************************
- * ADS 1115 Angle of Attack Class for Ardupilot platform
- * Author: John Ink
- * Date: 11 February 2021
- * File: ADS1115_AOA.cpp
- * Version 1.00
- *
- * Description:  
- *
-***************************************************/
-
-// #include <AP_HAL/AP_HAL.h>
 #include <AP_Logger/AP_Logger.h>
 #include <AP_HAL_ChibiOS/I2CDevice.h>
 #include <AP_HAL_ChibiOS/I2CDevice.cpp>
 
 #include "AP_AOA.h"
-
 #include <unistd.h>
 
 AP_AOA::AP_AOA()
@@ -27,11 +14,6 @@ void AP_AOA::init_adc(void)
 {   
     if (init()) {
         hal.console->printf("ADC init success.\n");
-        // if (start_conversion()) {
-        //     hal.console->printf("Conversion success.\n");
-        // } else {
-        //     hal.console->printf("Conversion fail.\n");
-        // }
     } else {
         hal.console->printf("ADC init fail.\n");
     }
